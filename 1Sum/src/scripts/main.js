35f67598a224f0ff41c946517f61fa4966df741f
Cecilia C <you@example.com>
2018-02-19 12:36:20 -0500

    :star: add functionality to home notepad

--- 1Sum/src/scripts/main.js
+++ 1Sum/src/scripts/main.js
@@ -1,27 +1,43 @@
-
-
 /**
- * Cover/home page notepad
- * This allows the user to enter notes by pressing Enter
+ * NOTEPAD
+ * This allows the user to enter notes by pressing Enter or via Add button
+ * Instance variables:
+ * note: returns notepad form input
+ * input: add note form input
+ * notepad: entire note area
  * 
  */
+function addNote() {
+    var note = $("#home-note-input").val();
+    var input = $("home-note-input");
+    var notepad = $("#home-notepad");
 
-function addNote () {
-    var note = $("note-input").val();
-    var notepad = $("home-notepad");
-    // get note input field
-    // get ul from notepad
-    // add note input as list item to note card
     if ( note !== "") {
         // add value as list item to notepad
-        var elem = $("<li><li>").text(val);
-        append(elem).notepad;
+        var elem = $("<li><li>").text(note);
+        $(notepad).append(elem);
+        // clear input field on keyboard enter or button press enter
+        $(input).val("");
+        $("#home-form")[0].reset();
     }
-    // clear input field on keyboard enter or button press enter
 }
 
-function maxNotes () {
-    // set max number if notes in notecard to 10
+/* function maxNotes() {
+* limit user from submitting more notes than can comfortably fit in 
+* the home page notepad
 }
+*/
 
-$("home-add").on("click", addNote());
+
+// on document ready    
+$(function(){
+    $("#home-add").on("click", addNote);
+    $("input").on("keydown", function(event){
+        // returns which keyboard key was pressed for this event
+        // 13 is the keycode for Enter
+        if (event.which==13){
+            event.preventDefault();
+            addNote();
+        }
+    });     
+})  
