9f236d17378cad5555155082216d0c225d25881b
Cecilia Constantine <you@example.com>
2018-04-16 21:07:04 -0400

    simplified directory structure, add framework for app functions, add db file

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,7 +3,71 @@
 // when the note is added
 // the time of the add must be saved
 // and the value added to the card footer
-function timestamp() {
-    var date = newDate(month, day, year, hours, minutes);
-    
-}
+
+(function() { //anonymous function - has priority
+    'use strict'; // enables built-in jslinting
+
+    // TODO: load empty card graphic instead of spinner of death
+    var app = {
+       // isLoading = true,
+        visibleCards: {},
+        spinner: document.querySelector(".spinner"),
+        cardTemplate: document.querySelector(".cardTemplate"),
+        addDialog: document.querySelector(".dialog-container"),
+    }
+
+
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+     // Refresh button
+    document.getElementById("refresh-btn").addEventListener("click", function() {
+      app.updateNotebook();
+     });
+
+    // Delete Note button
+    document.getElementById("delete-btn").addEventListener("click", function() {
+    app.deleteNote();
+    });  
+
+     // Create new card button
+     document.getElementById("add-Note").addEventListener("click", function() {
+    // open the new button dialog-container
+    //app.toggleAddNote(false);
+        timestamp();
+     });
+    // Save summary 
+     document.getElementById("add-Note").addEventListener("click", function() {
+    // save summary to summary page
+    });
+
+
+
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+  // Generate editable summary
+    document.getElementById("add-Note").addEventListener("click", function() {
+        // compile notes into unordered list to show to user
+        // open summary card dialog container
+        app.toggleAddNote(false);
+    });
+
+    function timestamp() {
+        var d = new Date();
+        var month = d.getMonth();
+        var monthNames = ["Jan", "Feb", "March", "April", "May", "June", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+        var thisMonth = monthNames[month];
+        var date = d.getDate();
+        document.getElementById("timestamp").innerHTML = thisMonth + " " + date; 
+    }   
+
+
+
+
+
+}) // end of anonymous function
---
+++ src/scripts/db.js
@@ -0,0 +1,11 @@
+// a Javascript module with a specific responsibility
+
+var sumDB = (function() { // anonymous function creates new module sumDB
+    var sDB = {}; // empty js object used to store all of the methods in teh module to be acessible from outside the scope of the module
+    var datastore = null;
+
+    //TODO: add methods for interacting with the database here
+
+    // export the tDB object
+    return sDB;
+}());
